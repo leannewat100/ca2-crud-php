37cfa6e63b86f3fc9f4b20ec44c1b8ffe4e1eee8
Leanne Watters <leannewat100@gmail.com>
2022-10-21 13:19:15 +0100

    edit

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ include('includes/header.php');
 <td><img src="image_uploads/<?php echo $record['image']; ?>" width="100px" height="100px" /></td>
 <td><?php echo $record['name']; ?></td>
 <td class="right"><?php echo $record['price']; ?></td>
-td><?php echo $record['location']; ?></td>
+<td><?php echo $record['location']; ?></td>
 <td><form action="delete_record.php" method="post"
 id="delete_record_form">
 <input type="hidden" name="record_id"
