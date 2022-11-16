c6a284e9e44935cb49de0a6ea5dd88ffd24d86fc
Leanne Watters <leannewat100@gmail.com>
2022-11-16 15:07:31 +0000

    radio buttons code

--- add_record.php
+++ add_record.php
@@ -10,7 +10,7 @@ $need = filter_input(INPUT_POST, 'need');
 $location = filter_input(INPUT_POST, 'location');
 
 // Validate inputs
-if ($category_id == null || $category_id == false || $name == null || $date == null) {
+if ($category_id == null || $category_id == false || $name == null || $date == null || $urgency == null) {
     $error = "Invalid product data. Check all fields and try again.";
     include('error.php');
     exit();
